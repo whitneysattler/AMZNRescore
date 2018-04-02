d6e7138e91c2433a8a1c294a18e68433bf1d103e
surreptitiousLizard <bax.nickolas@gmail.com>
2018-04-02 00:09:31 -0500

    Update getItemReviews.py
    Changed the way URI returns to a shorter version in order to reduce size of output. 
    Added support for gathering stars, item friendly-name  and posting date of the reviews
    New function for delineated output, but is triple pipe symbol delineated.

--- getItemReviews.py
+++ getItemReviews.py
@@ -1,11 +1,14 @@
-##########################
-#Author: NB. 3/29/18 - 11:55
-##########################
-
 from bs4 import BeautifulSoup
 import requests
 import re
 import time
+
+def printCsv(data):
+    csv="user|||userURI|||reviewText|||stars|||postDate|||productText\n"
+    for review in data:
+        csv+=review["user"]+"|||"+review["userURI"]+"|||"+review["reviewText"]+"|||"+review["stars"]+"|||"+review["postDate"]+"|||"+review["productText"]+"\n"
+    print(csv)
+
 
 def requestItemReviews(url,verbose):
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'}
@@ -13,14 +16,22 @@ def requestItemReviews(url,verbose):
     soup = BeautifulSoup(r.text,'html.parser')
     reviewList = []
     reviews = soup.find_all(attrs={'id': re.compile(r".*customer_review-.*")})
+    productText = soup.find(attrs={'class':"a-link-normal",'data-hook':"product-link"}).text
 
     for review in reviews:
         reviewSoup = BeautifulSoup(str(review),'html.parser')
         author = reviewSoup.find(attrs={'class':"a-size-base a-link-normal author"})
         reviewText = reviewSoup.find(attrs={'class':"a-size-base review-text"})
+        stars = reviewSoup.find(attrs={'class': "a-icon-alt"}).text[:1]
+        postDate = reviewSoup.find(attrs={'class': "a-size-base a-color-secondary review-date"}).text[3:]
         if verbose:
             print("\n#####################\n",author.text,"\n",author['href'],"\n",reviewText.text,"\n###################\n")
-        reviewList.append({"user":author.text, "userURI":author['href'], "reviewText":reviewText.text})
+        reviewList.append({"user":author.text,\
+                           "userURI":author['href'].split(".")[2].split("/")[0],\
+                           "reviewText":reviewText.text,\
+                           "stars":stars,\
+                           "postDate":postDate,\
+                           "productText":productText})
     return reviewList
 
 def gatherData(verbose):
@@ -31,7 +42,7 @@ def gatherData(verbose):
     while(pagesExist):
         time.sleep(2)
         if not(firstRun):
-            print(count)
+            print("Scraping Page: "+str(count))
             data = requestItemReviews(url + pageIterationTag + str(count), verbose)
             if len(data) == 0:
                 pagesExist = 0
@@ -49,4 +60,6 @@ def gatherData(verbose):
 url='https://www.amazon.com/William-Painter-Titanium-Polarized-Sunglasses/product-reviews/B01AH0PVMO/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
 pageIterationTag = "&pageNumber="
 verbose = 0
-print(gatherData(verbose))
+data = gatherData(verbose)
+print(data)
+#printCsv(data)
